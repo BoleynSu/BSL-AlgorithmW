5c4f69ef960ee2588bd54938d5a228c69eee7d0f
Boleyn Su <boleyn.su@gmail.com>
2016-12-16 00:48:40 +0800

    improve runtime without gc

--- rt/bsl_rt.h
+++ rt/bsl_rt.h
@@ -1,6 +1,19 @@
 #include <stdlib.h>
 
-#define BSL_RT_MALLOC malloc
+void *BSL_RT_MALLOC(size_t sz) {
+  static void *base, *top;
+  if (sz < top - base) {
+    return top -= sz;
+  } else {
+    base = malloc(1 << 23);
+    top = base + (1 << 23);
+    if (sz < top - base) {
+      return top -= sz;
+    } else {
+      return malloc(sz);
+    }
+  }
+}
 
 typedef void* BSL_RT_VAR_T;
 typedef BSL_RT_VAR_T (*BSL_RT_FUN_T)(BSL_RT_VAR_T, BSL_RT_VAR_T[]);
